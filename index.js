6908f1ad85ccdb416c7c69f2ec8cf99229e15687
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2025-01-08 18:09:49 +0600

    Main Structure

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+
+const categoryRoutes = require("./routes/category");
+const subcategoryRoutes = require("./routes/subCategory");
+const duaRoutes = require("./routes/dua");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("User Management");
+});
+
+// Routes
+app.use("/categories", categoryRoutes);
+app.use("/subcategories", subcategoryRoutes);
+app.use("/duas", duaRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
